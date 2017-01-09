f563eea36387a5f88626acf0218a371c12ccdabd
hoetmaaiers <houdmeyers@gmail.com>
2017-01-09 17:34:36 +0100

    initial commit

---
+++ src/find-and-replace.js
@@ -0,0 +1,23 @@
+export default class FindAndReplacer {
+    constructor() {
+        console.log('welcome this FindAndReplace instance');
+    }
+
+    replace(path, keyDefinitions) {
+        console.log('replace');
+
+        // read every directory in path
+        // for every directory check if the regex for variables matches
+    }
+
+    
+    replaceDirectoryName() {
+        // readdirSync();
+    }
+
+    replaceFileName() {
+    }
+
+    replaceContent() {
+    }
+}
